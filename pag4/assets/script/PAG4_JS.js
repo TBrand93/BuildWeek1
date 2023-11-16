b1ea46d146ca3c183835616d893738b322fc7f80
lindaweb92 <148793222+lindaweb92@users.noreply.github.com>
2023-11-16 15:36:21 +0100

    PAG4 ULTIME MODIFICHE

--- pag4/assets/script/PAG4_JS.js
+++ pag4/assets/script/PAG4_JS.js
@@ -1,22 +1,33 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var stars = document.querySelectorAll(".star");
+    const stars = document.querySelectorAll(".star");
 
-    stars.forEach(function (star, index) {
+    stars.forEach((star, starIndex) => {
+        star.addEventListener("mouseover", function () {
+            highlightStars(starIndex);
+        });
+
         star.addEventListener("click", function () {
-            resetStars();
-            highlightStars(index);
+            markStars(starIndex);
         });
     });
 
-    function resetStars() {
-        stars.forEach(function (star) {
-            star.classList.remove("checked");
+    function highlightStars(starIndex) {
+        stars.forEach((star, j) => {
+            if (j <= starIndex) {
+                star.classList.add("hovered");
+            } else {
+                star.classList.remove("hovered");
+            }
         });
     }
 
-    function highlightStars(index) {
-        for (var i = 0; i <= index; i++) {
-            stars[i].classList.add("checked");
-        }
+    function markStars(starIndex) {
+        stars.forEach((star, j) => {
+            if (j <= starIndex) {
+                star.classList.add("clicked");
+            } else {
+                star.classList.remove("clicked");
+            }
+        });
     }
 });
