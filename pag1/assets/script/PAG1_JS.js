3b024e6a9750186ab3c247455762cb09fe4f1a47
TBrand93 <thomasbrand993@gmail.com>
2023-11-16 15:51:38 +0100

    collegamento pagine

--- pag1/assets/script/PAG1_JS.js
+++ pag1/assets/script/PAG1_JS.js
@@ -3,21 +3,19 @@ document.addEventListener("DOMContentLoaded", function () {
     var checkbox = document.getElementById("myCheckbox");
     var proceedButton = document.querySelector(".btn");
 
-    // Aggiungi un gestore di eventi per il cambio di stato del checkbox
-    checkbox.addEventListener("change", function () {
-        // Abilita/disabilita il bottone in base allo stato del checkbox
-        proceedButton.disabled = !checkbox.checked;
-    });
+  
 
     // Aggiungi un gestore di eventi per il click sul bottone
     proceedButton.addEventListener("click", function () {
         // Controlla se il checkbox è selezionato
         if (checkbox.checked) {
             // Se il checkbox è selezionato, il link ti porterà a pagina2.html
-            window.location.href = "https://www.google.it";
+            window.location.href = "/pag 2-3/PAG2_3.html";
         } else {
             // Se il checkbox non è selezionato, mostra un avviso
             alert("Devi selezionare la casella di controllo per procedere.");
         }
     });
 });
+
+
