f0a98bbb2d91a0a786ada90b76ec7649f919bb0c
TunderBorn <orione.pegaso@gmail.com>
2023-11-17 17:12:05 +0100

    Pagina_2_Codice_per_il_Quiz

--- pag1/assets/script/PAG1_JS.js
+++ pag1/assets/script/PAG1_JS.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function () {
         // Controlla se il checkbox è selezionato
         if (checkbox.checked) {
             // Se il checkbox è selezionato, il link ti porterà a pagina2.html
-            window.location.href = "/pag 2-3/PAG2_3.html";
+            window.location.href = "../pag 2-3/PAG2_3.html";
         } else {
             // Se il checkbox non è selezionato, mostra un avviso
             alert("Devi selezionare la casella di controllo per procedere.");
